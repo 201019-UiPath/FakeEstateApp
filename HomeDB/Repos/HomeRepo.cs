32c0c8bab39e16010e5b6e5d9d07d3cd7e700882
Shantel Chi <schi3@masonlive.gmu.edu>
2020-11-12 10:27:53 -0500

    More repoo

--- HomeDB/Repos/HomeRepo.cs
+++ HomeDB/Repos/HomeRepo.cs
@@ -1,7 +1,23 @@
-namespace HomeDB.Repos
+using System.Collections.Generic;
+using System;
+using HomeDB.Models;
+using System.Linq;
+using HomeDB.Entities;
+using HomeDB.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeDB
 {
     public class HomeRepo : IRepo
     {
+        private HomeContext context;
+        private IMapper mapper;
+        
+        public HomeRepo (HomeContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
         
     }
 }
