4c2f4e8cf494f1deda4e21d03508658ccbdfb32c
Shantel Chi <schi3@masonlive.gmu.edu>
2020-11-12 10:00:13 -0500

    Models

---
+++ HomeDB/Models/House.cs
@@ -0,0 +1,13 @@
+namespace HomeDB.Models
+{
+    public class House
+    {
+        public int HouseId {get; set;}
+        public int Bedrooms {get; set;}
+        public int Floors {get; set;}
+        public string Location {get; set;}
+        public bool AC {get; set;}
+        public bool Heating {get; set;}
+        public decimal Price {get; set;}
+    }
+}
