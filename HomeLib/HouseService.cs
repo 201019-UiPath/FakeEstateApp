454e5a6f7fe820928372c2f4310f1d2e1acd4b7c
Andres Siles-Loayza <andres.siles-loayza@revature.net>
2020-11-12 10:51:55 -0500

    created solution and services library

---
+++ HomeLib/HouseService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeLib
+{
+    public class HomeService
+    {
+        private IHouseRepo _repo;
+
+        public HomeService(IHouseRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public void AddHouse(House house)
+        {
+            _repo.AddHouse(house);
+        }
+
+        public List<House> GetAllHouses()
+        {
+            _repo.GetAllHouses();
+        }
+    }
+}
