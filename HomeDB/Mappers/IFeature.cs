d7c09e6d70a364f4721208de1080a2c5e4420a4c
Shalei Kumar <shaleik@bu.edu>
2020-11-12 10:37:33 -0500

    Started Mappers

--- HomeDB/Mappers/IFeature.cs
+++ HomeDB/Mappers/IFeature.cs
@@ -1,7 +1,14 @@
+using HomeDB.Entities;
+using HomeDB.Models;
+using System.Collections.Generic;
+
 namespace HomeDB.Mappers
 {
     public interface IFeature
     {
-         
+        Features ParseFeature(Feature feature);
+        ICollection<Features> ParseFeaturer(List<Feature> feature);
+        Feature ParseFeature(Features features);
+        List<Feature> ParseFeature(ICollection<Features> features);
     }
 }
--- HomeDB/Mappers/IHouseMapper.cs
+++ HomeDB/Mappers/IHouseMapper.cs
@@ -1,7 +1,12 @@
+using HomeDB.Entities;
+using HomeDB.Models;
+using System.Collections.Generic;
+
 namespace HomeDB.Mappers
 {
     public interface IHouseMapper
     {
-         
+        Houses ParseHouse(Houses houses);
+        
     }
 }
