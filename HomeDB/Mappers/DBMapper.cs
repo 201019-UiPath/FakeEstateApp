e997f75ab0591863b29260afe006c884de6cb91c
Shantel Chi <schi3@masonlive.gmu.edu>
2020-11-12 11:35:36 -0500

    More small fixes

--- HomeDB/Mappers/DBMapper.cs
+++ HomeDB/Mappers/DBMapper.cs
@@ -53,7 +53,7 @@ namespace HomeDB.Mappers
             return new House(){
                 HouseId = houses.Id,
                 Bedrooms = Convert.ToInt32(houses.Bedrooms),
-                Bathrooms = Convert.ToInt32(houses.Bathrooms),
+                Bathroom = Convert.ToInt32(houses.Bathrooms),
                 Floors = Convert.ToInt32(houses.Floors),
                 Location = houses.Location,
                 AC = Convert.ToBoolean(houses.Ac),
--- HomeDB/Models/House.cs
+++ HomeDB/Models/House.cs
@@ -3,11 +3,12 @@ namespace HomeDB.Models
     public class House
     {
         public int HouseId {get; set;}
-        public int Bedrooms {get; set;}
-        public int Floors {get; set;}
+        public int? Bathrooms {get; set;}
+        public int? Bedrooms {get; set;}
+        public int? Floors {get; set;}
         public string Location {get; set;}
-        public bool AC {get; set;}
-        public bool Heating {get; set;}
-        public decimal Price {get; set;}
+        public bool? AC {get; set;}
+        public bool? Heating {get; set;}
+        public decimal? Price {get; set;}
     }
 }
