c333556c94cb3d46c9997d8c161f796a77bd455b
Shantel Chi <schi3@masonlive.gmu.edu>
2020-11-12 10:03:05 -0500

    Model2

---
+++ HomeDB/Models/HouseFeatures.cs
@@ -0,0 +1,9 @@
+namespace HomeDB.Models
+{
+    public class HouseFeatures
+    {
+        public int HouseFeatureId {get; set;}
+        public int HouseId {get; set;}
+        public int FeatureId {get; set;}
+    }
+}
