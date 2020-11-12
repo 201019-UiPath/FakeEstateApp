53c48d481d3d318df025a6496c1413e280b0b12c
Andres Siles-Loayza <andres.siles-loayza@revature.net>
2020-11-12 16:28:00 -0500

    separated html and js files

---
+++ WebUI/designhouse.js
@@ -0,0 +1,31 @@
+
+function AddHouse()
+{
+    let house = {};
+    house.bedrooms = parseInt( document.querySelector('#bedrooms').value);
+    house.bathrooms = parseInt(document.querySelector('#bathrooms').value);
+    house.floors = parseInt(document.querySelector('#floors').value);
+    house.location = document.querySelector('#location').value;
+    house.ac = "true"==document.querySelector('#ac').value;
+    house.heating = "true"==document.querySelector('#heating').value;
+    house.price = parseInt(document.querySelector('#price').value);
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status > 199 && this.status < 300)
+        {
+            alert("New house added!");
+            document.querySelector('#bedrooms').value = '';
+            document.querySelector('#bathrooms').value= '';
+            document.querySelector('#floors').value= '';
+            document.querySelector('#location').value= '';
+            document.querySelector('#ac').value= '';
+            document.querySelector('#heating').value= '';
+            document.querySelector('#price').value= '';
+        }
+    };
+    xhr.open("POST", 'https://localhost:44341/House/AddHouse', false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(house));
+}
+
