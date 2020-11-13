3bbe6716823f7ac70f6328f1d71d6050d5c6022e
Andres Siles-Loayza <andres.siles-loayza@revature.net>
2020-11-12 22:48:16 -0500

    reformatted house features, etc.

--- WebUI/ourhouses.js
+++ WebUI/ourhouses.js
@@ -12,16 +12,16 @@ function GetAllHouses()
             cell0.innerHTML = result[i].houseId;
 
             let cell1 = row.insertCell(1);
-            cell1.innerHTML = result[i].bedrooms;
+            cell1.innerHTML = result[i].location;
 
             let cell2 = row.insertCell(2);
-            cell2.innerHTML = result[i].bathrooms;
+            cell2.innerHTML = result[i].bedrooms;
 
             let cell3 = row.insertCell(3);
-            cell3.innerHTML = result[i].floors;
+            cell3.innerHTML = result[i].bathrooms;
 
             let cell4 = row.insertCell(4);
-            cell4.innerHTML = result[i].location;
+            cell4.innerHTML = result[i].floors;
 
             let cell5 = row.insertCell(5);
             if (result[i].ac)
@@ -34,7 +34,7 @@ function GetAllHouses()
             else {cell6.innerHTML = 'No';}
 
             let cell7 = row.insertCell(7);
-            cell7.innerHTML = '$'+numberWithCommas(result[i].price);
+            cell7.innerHTML = '$'+result[i].price.toLocaleString();
 
             //create button to access features 
             ///*
@@ -43,8 +43,4 @@ function GetAllHouses()
             document.getElementById('btn'+i).onclick = () => window.location.href = "viewhouse.html?"+result[i].houseId;
         }
     });
-}
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
--- WebUI/viewhouse.js
+++ WebUI/viewhouse.js
@@ -3,7 +3,7 @@ function GetHouse()
     fetch('https://localhost:44341/House/GetHouse?houseId='+location.search.substring(1)) //fetch based on URL houseId
     .then(response => response.json())
     .then(result => {
-        //document.querySelector('#houseid').innerHTML = 'ID: '+result.houseId;
+        document.querySelector('#houseid').innerHTML = 'Viewing House with ID: '+result.houseId;
         document.querySelector('#bedrooms').innerHTML = 'Bedrooms: '+result.bedrooms;
         document.querySelector('#bathrooms').innerHTML = 'Bathrooms: '+result.bathrooms;
         document.querySelector('#floors').innerHTML = 'Floors: '+result.floors;
@@ -14,7 +14,7 @@ function GetHouse()
         if (result.heating)
         {document.querySelector('#heating').innerHTML = 'Heating: '+'Yes';}
         else {document.querySelector('#heating').innerHTML = 'Heating: '+'No';}
-        document.querySelector('#price').innerHTML = 'Price: $'+numberWithCommas(result.price);
+        document.querySelector('#price').innerHTML = 'Price: $'+result.price.toLocaleString();
 
         document.querySelectorAll('#features li').forEach(element => element.remove()); //clear unordered list
         let listElement = document.querySelector('#features'); //gets unordered list
@@ -34,8 +34,4 @@ function GetHouse()
 
         }
     });
-}
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
